917dd383ea4bf05c07ccf60ac20d4ccc52ffeb3a
ajakaiye enitan <ajakaiyeenitan@gmail.com>
2022-10-16 11:15:41 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int num;
 
-	for (num = '0' num <= '9'; num++)
+	for (num = '0'; num <= '9'; num++;)
 	{
 		putchar(num);
 		if (num == '9')
@@ -17,7 +17,7 @@ int main(void)
 		else
 		{
 		putchar(',');
-		putchat(' ')
+		putchar(' ');
 		}
 	}
 	putchar('\n');
