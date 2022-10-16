fa6d597a945e4af833ee21d1c42cab73f1dd7517
ajakaiye enitan <ajakaiyeenitan@gmail.com>
2022-10-16 10:09:16 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <sydio.h>
+/**
+ *main-Entry point
+ *Return:0 if no error, non zero if error.
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i '0' : i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
