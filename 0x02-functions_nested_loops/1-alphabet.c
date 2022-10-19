e64ff10fa248e2e4211d1921a07ace0e4561cf49
ajakaiye enitan <ajakaiyeenitan@gmail.com>
2022-10-19 13:13:36 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *main - prints the alphabet in lowercase
+ * print_alphabet - prints the alphabet in lowercase
  *Return: Always 0
 */
-int main(void)
+void print_alphabet(void)
 {
 	char la;
 
