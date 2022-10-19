15b1069698f1b09fa9671cee6ecef059fc56b5e3
ajakaiye enitan <ajakaiyeenitan@gmail.com>
2022-10-19 13:23:22 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prits alpha 10 times
+ * print_alphabet_x10 - prints alpha 10 times
 */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
 
+	for ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putcha(la);
+		_putchar('\n');
+	}
+}
