f2dfff2563b3a45e58bd1610f8705f2ce4d78267
ajakaiye enitan <ajakaiyeenitan@gmail.com>
2022-10-19 11:53:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - prints the alphabet in lowercase
+ *Return: Always o
+*/
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n')
+}
