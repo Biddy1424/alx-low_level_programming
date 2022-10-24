a38e60d7f9c66447ae9e7406b48e896e4f4516ef
ajakaiye enitan <ajakaiyeenitan@gmail.com>
2022-10-24 14:29:54 -0700

    real 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,11 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * @s: The string to get the length of.
  *
  * Return: The length of @str.
 */
-int _strlen(char *str)
+int _strlen(char *s)
 {
 	int len = 0;
 
